8870f227f32f46f9eab90be2b75ea13a6cdfe01b
Ryma Djoudad <zenyod85@gmail.com>
2026-03-15 13:03:25 -0400

    Fixed 9-2

--- restaurant.py
+++ restaurant.py
@@ -5,7 +5,7 @@ class Restaurant:
         self.cuisine_type = cuisine_type
         self.number_served = 0
 
-    def describe_restaurant(restaurant_type, cuisine_type):
+    def describe_restaurant(self, restaurant_type, cuisine_type):
         print(f"This is a {restaurant_type} restaurant. We make {cuisine_type} dishes.")
 
     def set_number_served(self, number_served):
@@ -22,4 +22,10 @@ print(tensuke_express.number_served)
 tensuke_express.set_number_served(0)
 print(tensuke_express.number_served)
 
+olive_garden = Restaurant("Olive Garden", "Italian")
+the_avenue = Restaurant("The Avenue", "Steakhouse")
+bonifacio = Restaurant("Bonifacio", "Filipino")
 
+olive_garden.describe_restaurant("Pasta", "Italian")
+the_avenue.describe_restaurant("Steak", "American")
+bonifacio.describe_restaurant("Filipino", "Filipino")
